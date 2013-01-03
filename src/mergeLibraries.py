445fb676d5dd665ce2dd09064338507f812f3a2b
Birk Bremer <bbprojects2013@gmail.com>
2013-01-03 17:13:59 +0100

    Intermediate Commit - backup

--- src/mergeLibraries.py
+++ src/mergeLibraries.py
@@ -1,12 +1,15 @@
 #coding: latin-1
 
 import sys
-
+import shutil       #for copy
 import xml.etree.ElementTree as ET
 
 #This is a small program enabling to transfer the rating and the playcount of different iTunes to another one
 #This Software is published under the GPL3 License. 
 #Copyright 2013 Birk Bremer
+
+
+
  
 class MusicDBclass(dict):
     def __init__(self):
@@ -31,6 +34,15 @@ class MusicDBclass(dict):
         sSongName_normalized = self.__normalize(sSongName)
         
         return "%s%s"%(sArtist_normalized,sSongName_normalized)
+    
+    def GetValue(self, sArtist, sSongName):
+        sHash = self.__CalcHash(sArtist, sSongName)
+        
+        if not self.has_key(sHash):
+            return None, None
+        else:
+            return self[sHash]["iCount"], self[sHash]["iRating"] 
+        
     
     def add(self, sArtist, sSongName, iCount, iRating):
         '''
@@ -58,22 +70,15 @@ class MusicDBclass(dict):
         if iRating != None:
             if self[sHash]["iRating"] < iRating:
                 self[sHash]["iRating"] = iRating
-            
+   
+MusicDB = MusicDBclass()         
         
-        
-MusicDB = MusicDBclass()
 
-
-def ParseTunesXML(sLibraryFile):
-    global MusicDB
-    #this function does the actual XML Parsing and creates the LibraryObjects
-    print "Parsing Library File %s"%(sLibraryFile)
-    #fh_LibraryFile = open(sLibraryFile,'r')
-    #xml_data = fh_LibraryFile.read()
-    #fh_LibraryFile.close()
-    
-    
-    tree = ET.parse(sLibraryFile)
+def GetItunesTracks(tree):
+    '''
+    Extracts all the iTunes Tracks elements and returns a list with them (wired xml usage!)
+    @param tree: the xml tree element
+    '''
     root = tree.getroot()
     
     tracks = []
@@ -81,7 +86,7 @@ def ParseTunesXML(sLibraryFile):
     for idx, child in enumerate(root[0]):
         if child.text == "Tracks":
             #print child.text, child.tag, child.attrib, "\n"
-            print "Found Tracks root"
+            #print "Found Tracks root"
             tracks_root = root[0][idx+1]
             
             #extract all track elements
@@ -90,6 +95,19 @@ def ParseTunesXML(sLibraryFile):
                     tracks.append(track)
                 #print track.tag, track.text
             break
+    return tracks
+
+def ParseTunesXML(sLibraryFile):
+    global MusicDB
+    #this function does the actual XML Parsing and creates the LibraryObjects"
+    print "Reading Library File %s"%(sLibraryFile)
+    #fh_LibraryFile = open(sLibraryFile,'r')
+    #xml_data = fh_LibraryFile.read()
+    #fh_LibraryFile.close()
+    
+    
+    tree = ET.parse(sLibraryFile)
+    tracks = GetItunesTracks(tree)
     
     for track in tracks:
         #parse all the tracks element and transfer the information to the  summary
@@ -115,10 +133,57 @@ def ParseTunesXML(sLibraryFile):
             if sTrackinfo.text == "Play Count":
                 #print "found %s"%track[idx+1].text
                 iCount = int(track[idx+1].text)
-        print "Song: %s by %s was played %s and rated %s"%(sSongName, sArtist, iCount, iRating)
+        #print "Song: %s by %s was played %s and rated %s"%(sSongName, sArtist, iCount, iRating)
         MusicDB.add(sArtist, sSongName, iCount, iRating)
-    print MusicDB
+    #print MusicDB
         
+def UpdateTunesDB(sLibraryFile):
+    global MusicDB
+    #this function Modifies the iTunes Library
+    
+    print "Create a backup of %s (is now %s_backup)"%(sLibraryFile,sLibraryFile)
+    shutil.copyfile(sLibraryFile, "%s_backup"%(sLibraryFile))
+    
+    print "Writing Library File %s"%(sLibraryFile)
+    
+    tree = ET.parse(sLibraryFile)
+    root = tree.getroot()
+    tracks = GetItunesTracks(tree)    
+    
+    for track in tracks:
+        for idx, sTrackinfo in enumerate(track):
+            #print sTrackinfo.tag, sTrackinfo.text
+            
+            if sTrackinfo.text == "Name":
+                #print "found %s"%track[idx+1].text
+                sSongName = track[idx+1].text
+                
+            if sTrackinfo.text == "Artist":
+                #print "found %s"%track[idx+1].text
+                sArtist = track[idx+1].text
+                
+            if sTrackinfo.text == "Rating":
+#                print "Del tag: %s, Text: %s"%(sTrackinfo.tag, sTrackinfo.text)
+                track.remove(sTrackinfo)
+                track.remove(track[idx])
+                #bDelnext = True
+                
+            
+            if sTrackinfo.text == "Play Count":
+ #               print "Del tag: %s, Text: %s"%(sTrackinfo.tag, sTrackinfo.text)
+                track.remove(sTrackinfo)
+                track.remove(track[idx])
+    
+        if MusicDB.GetValue(sArtist, sSongName) != (None, None):
+            iCount, iRating = MusicDB.GetValue(sArtist, sSongName)
+            print "Updating: %s / %s with Count: %s, Raing: %s"%(sArtist, sSongName, iCount, iRating)
+            
+                
+
+                
+                
+    tree.write(r"./sample/testfile.xml")
+                
 
 
 
@@ -138,6 +203,8 @@ def main():
     for sSourcefile in sSourceFiles:
         #parse all the input Mediatheks
         ParseTunesXML(sSourcefile)
+        
+    UpdateTunesDB(sDestinFile)
         
         
     
