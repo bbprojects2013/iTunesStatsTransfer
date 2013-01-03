42a91a3b1db31dac2bb6502162761f79dad87515
Birk Bremer <bbprojects2013@gmail.com>
2013-01-03 13:52:15 +0100

    Added Copyright

--- src/mergeLibraries.py
+++ src/mergeLibraries.py
@@ -1,5 +1,11 @@
 
 
+import sys
+
+#This is a small program enabling to transfer the rating and the playcount of different iTunes to another one
+#This Software is published under the GPL3 License. 
+#Copyright 2013 Birk Bremer
+ 
 
 
 
@@ -10,10 +16,19 @@
 
 
 
-
-
-
-
+def main():
+    #just the main function
+    if len(sys.argv) == 1: 
+        sSourceFiles = [r"./sample/Mediathek.xml"]
+        sDestinFile = r"./sample/Mediathek_dst.xml"
+        
+        print "Testmode: Using as Input: %s and output: %s"%(sSourceFiles, sDestinFile)
+        
+    for sSourcefile in sSourceFiles:
+        pass
+        #parse all the input Mediatheks and transfer the read data in our librarya
+        
+    
 
 
 
