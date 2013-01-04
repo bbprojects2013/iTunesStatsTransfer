44803559cafef31aeb70b6ae434668cc89254643
Birk Bremer <bbprojects2013@gmail.com>
2013-01-04 01:31:13 +0100

    Added documentation to the README and documented / beautified the code

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 iTunesStatsTransfer
 ===================
 
-Some small Scripts in Python to for transferring Rating and Playcount to another iTunes Library
+Some small Scripts in Python to for transferring rating and playcount to another iTunes Library
+
+Usage: 
+1) Make a Backup of all your iTunes stuff - this software comes without any guaranty and you might loose everything!
+2) Export the Media XML in the different Itunes libraries using Storage -> Mediatek -> Export Mediatek
+3) Merge the different XML using: python mergeLibraries.py mediatek1.xml <mediatek2.xml> <mediatek_n.xml> destination_mediatek.xml
+4) In the destination Library remove all Music from the library but DON'T DELETE the music from the hard disk
+5) Go to Storage -> Mediatek -> import Mediatek and select the file destination_mediatek.xml
+6) All your Music including the updated ratings and play counts should be back
+7) Remove the duplicated play lists
--- src/mergeLibraries.py
+++ src/mergeLibraries.py
@@ -1,14 +1,15 @@
-#coding: latin-1
-
-import sys
-import shutil       #for copy
-import xml.etree.ElementTree as ET
+#coding: utf-8
 
 #This is a small program enabling to transfer the rating and the playcount of different iTunes to another one
-#This Software is published under the GPL3 License. 
-#Copyright 2013 Birk Bremer
+#This Software is published under the GPL3 License and comes "as it is" without any warranty
+#Copyright by Birk Bremer, 2013
 
 
+
+import sys
+import os
+import shutil       #for copy
+import xml.etree.ElementTree as ET
 
  
 class MusicDBclass(dict):
@@ -20,7 +21,7 @@ class MusicDBclass(dict):
         Removes special characters, spaces and decapitalizes everything
         @param string: string to work on
         '''
-        string = string.replace("Š","ae").replace("š","oe").replace("Ÿ","ue").replace("§","ss").replace("Ÿ","ue").replace("_","").replace(" ","_").replace("-","").replace("/","").replace("\\","").lower()
+        string = string.replace(u"Ã¤","ae").replace(u"Ã¶","oe").replace(u"Ã¼","ue").replace(u"ÃŸ","ss").replace("_","").replace(" ","_").replace("-","").replace("/","").replace("\\","").lower()
         return string
         
     def __CalcHash(self, sArtist, sSongName):
@@ -136,21 +137,38 @@ def ParseTunesXML(sLibraryFile):
         #print "Song: %s by %s was played %s and rated %s"%(sSongName, sArtist, iCount, iRating)
         MusicDB.add(sArtist, sSongName, iCount, iRating)
     #print MusicDB
+    
+def WriteItunesXML(XMLobj, FilePathName):
+    '''
+    Writes the given XML object to a iTunes compatible file
+    @param XMLobj: The xml object in form of a element tree (root object)
+    '''
+    out = '<?xml version="1.0" encoding="UTF-8"?>\n'
+    out += '<!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">\n'
+    out += ET.tostring(XMLobj)
+    
+    FilePathName = os.path.abspath(FilePathName)
+    
+    fh = open(FilePathName,"w")
+    print "Writing file %s"%(FilePathName)
+    fh.write(out)
+    fh.close()
         
-def UpdateTunesDB(sLibraryFile):
+def UpdateTunesDB(sDestinFile):
     global MusicDB
     #this function Modifies the iTunes Library
     
-    print "Create a backup of %s (is now %s_backup)"%(sLibraryFile,sLibraryFile)
-    shutil.copyfile(sLibraryFile, "%s_backup"%(sLibraryFile))
+    print "Create a backup of %s (is now %s.backup)"%(sDestinFile,sDestinFile)
+    shutil.copyfile(sDestinFile, "%s.backup"%(sDestinFile))
     
-    print "Writing Library File %s"%(sLibraryFile)
+    print "Parsing File %s for updates"%(sDestinFile)
     
-    tree = ET.parse(sLibraryFile)
+    tree = ET.parse(sDestinFile)
     root = tree.getroot()
     tracks = GetItunesTracks(tree)    
     
     for track in tracks:
+        #Search for the reqired Elements
         for idx, sTrackinfo in enumerate(track):
             #print sTrackinfo.tag, sTrackinfo.text
             
@@ -162,18 +180,20 @@ def UpdateTunesDB(sLibraryFile):
                 #print "found %s"%track[idx+1].text
                 sArtist = track[idx+1].text
                 
+            #remove a existing Rating
             if sTrackinfo.text == "Rating":
 #                print "Del tag: %s, Text: %s"%(sTrackinfo.tag, sTrackinfo.text)
                 track.remove(sTrackinfo)
                 track.remove(track[idx])
                 #bDelnext = True
                 
-            
+            #remove a existing Playcount    
             if sTrackinfo.text == "Play Count":
  #               print "Del tag: %s, Text: %s"%(sTrackinfo.tag, sTrackinfo.text)
                 track.remove(sTrackinfo)
                 track.remove(track[idx])
     
+        #if we have a updated rating put it to the x,l structure
         if MusicDB.GetValue(sArtist, sSongName) != (None, None):
             iCount, iRating = MusicDB.GetValue(sArtist, sSongName)
             print "Updating: %s / %s with Count: %s, Raing: %s"%(sArtist, sSongName, iCount, iRating)
@@ -190,49 +210,49 @@ def UpdateTunesDB(sLibraryFile):
                 num_rating = ET.SubElement(track, "integer")
                 num_rating.text = "%s"%(iRating)
                 
-                
-            
-         
-            
-            
-                
-
-    out = '<?xml version="1.0" encoding="UTF-8"?>\n'
-    out += '<!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">\n'
-    out += ET.tostring(root)
-    fh = open(r"./sample/testfile.xml","w")
-    fh.write(out)
-    fh.close()
-
-    #print ET.tostring(root)
-    #tree.write(r"./sample/testfile.xml")
-                
+    WriteItunesXML(root, sDestinFile)
 
 
-
-
-
-
-
-
-def main():
-    #just the main function
-    if len(sys.argv) == 1: 
-        sSourceFiles = [r"./sample/Mediathek.xml", r"./sample/Mediathek_dst.xml"]
-        sDestinFile = r"./sample/Mediathek_dst.xml"
-        
-        print "Testmode: Using as Input: %s and output: %s"%(sSourceFiles, sDestinFile)
-        
+def main(sSourceFiles, sDestinFile):
+    
     for sSourcefile in sSourceFiles:
         #parse all the input Mediatheks
+        sSourcefile = os.path.abspath(sSourcefile)
+        if not os.path.exists(sSourcefile):
+            print "File %s doesn't exist --> Exit"
+            sys.exit(1)
         ParseTunesXML(sSourcefile)
-        
-    UpdateTunesDB(sDestinFile)
-        
-        
     
+    sDestinFile = os.path.abspath(sDestinFile)
+    UpdateTunesDB(sDestinFile)
+    sys.exit(0)
+
 
 if __name__ == "__main__":
-    main()
-
-#test
+    #called in case this file is called directly
+    
+    #test if we need to print the help
+    if (len(sys.argv) == 1) or (sys.argv[1] == "-h") or (sys.argv[1] == "--help"): 
+        print "Usage: python %s mediatek1.xml <mediatek2.xml> <mediatek_n.xml> destination_mediatek.xml"%(os.path.split(sys.argv[0])[1])
+        print "\n\nLong explanation"
+        print "1) Make a Backup of all your iTunes stuff - this software comes without any guaranty and you might loose everything!"
+        print "2) Export the Media XML in the different Itunes libraries using Storage -> Mediatek -> Export Mediatek"
+        print "3) Merge the different XML using: python %s mediatek1.xml <mediatek2.xml> <mediatek_n.xml> destination_mediatek.xml"%(os.path.split(sys.argv[0])[1])
+        print "4) In the destination Library remove all Music from the library but DON'T DELETE the music from the hard disk"
+        print "5) Go to Storage -> Mediatek -> import Mediatek and select the file destination_mediatek.xml"
+        print "6) All your Music including the updated ratings and play counts should be back"
+        print "7) Remove the duplicated play lists"
+        sys.exit(0)
+        
+    if sys.argv[1] == "-t":
+        sSourceFiles = [r"./sample/Mediathek.xml", r"./sample/Mediathek_dst.xml"]
+        sDestinFile = r"./sample/testfile.xml"
+        print "Testmode: Using as Input: %s and output: %s"%(sSourceFiles, sDestinFile)
+        main(sSourceFiles, sDestinFile)
+        sys.exit(0)
+        
+    
+    sSourceFiles = (sys.argv[1:])
+    sDestinFile = sys.argv[-1] 
+    
+    main(sSourceFiles, sDestinFile)
